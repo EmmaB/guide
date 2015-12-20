678d1bb3b40d9723c222a98ea27f9263dc6f6d1b
Emma Barnes <emma@snowbooks.com>
2015-12-20 17:19:11 +0000

    toc remove

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -7,3 +7,13 @@ PDFKit.configure do |config|
   # if RAILS_ENV == 'production'
   config.verbose = true
 end
+
+if Rails.env.production?
+  font_dir = File.join(Dir.home, ".fonts")
+  Dir.mkdir(font_dir) unless Dir.exists?(font_dir)
+
+  Dir.glob(Rails.root.join("vendor","assets","fonts", "*")).each do |font|
+    target = File.join(font_dir, File.basename(font))
+    File.symlink(font, target) unless File.exists?(target)
+  end
+end
