14b4f9bab1445d2a60ae2d7f9c223847488ebb5d
Emma Barnes <emma@snowbooks.com>
2015-12-20 16:40:32 +0000

    toc remove

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Guide::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
